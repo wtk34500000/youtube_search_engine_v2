a97a075af39a915768f5f39f1c9818d659051e68
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2018-11-07 22:44:29 -0500

    day 2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react'
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
+import VideoDetail from "./components/video_detail"
 const API_KEY= 'AIzaSyAyxwNeDJwwYYLA33BYli6iJm2i1dbfVug';
 
-YTSearch({key:API_KEY, term:'surboards'},function(data){
-	console.log(data);
-})
+class App extends Component{
+	constructor(props){
+		super(props);
 
-const App = function(){
-	return (
-		<div>
-			<SearchBar />
-		</div>
-		);
+		this.state={videos: [] };
+
+		YTSearch({key:API_KEY, term:'birds'}, (videos)=>{
+			this.setState({videos})
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]}/>
+				<VideoList videos={this.state.videos} />
+			</div>
+		)
+	}
+	
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
